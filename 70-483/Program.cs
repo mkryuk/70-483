534210549d3d20358330587ab885d16e60d4e824
mkryuk <mkryuk@gmail.com>
2014-12-10 14:42:30 +0200

    Obj 1.1. Threads

---
+++ 70-483/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _70_483
+{
+
+    class Program
+    {
+        [ThreadStatic]
+        public static int _field;
+
+        public static ThreadLocal<int> _field2 = new ThreadLocal<int>(() => Thread.CurrentThread.ManagedThreadId);
+
+        static void Main(string[] args)
+        {            
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field2.Value; i++)
+                {                    
+                    _field++;
+                    Console.WriteLine(Thread.CurrentThread.CurrentCulture.TextInfo.ToString() + _field);
+                    
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < _field2.Value; i++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread 2: " + _field);
+                }
+            }).Start();
+        }
+    }
+}
