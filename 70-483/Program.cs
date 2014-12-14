e0d3a7e9ebe91d61173a9e816e840b9a7ec5f423
maxim <mkryuk@gmail.com>
2014-12-14 13:49:00 +0200

    Obj 1.4. Event add remove

--- 70-483/Program.cs
+++ 70-483/Program.cs
@@ -4,42 +4,46 @@ using System.Threading.Tasks;
 
 namespace _70_483
 {
-
-    class Program
+    public class SomeClass
     {
+        private event EventHandler someEvent = delegate { };
 
+        public event EventHandler SomeEvent
+        {
+            add
+            {
+                lock (someEvent)
+                {
+                    someEvent += value;
+                } 
+            }
+            remove
+            {
+                lock (someEvent)
+                {
+                    someEvent -= value;
+                }
+            }
+        }
+
+        public void DoSomeWork( EventHandler _event)
+        {
+            SomeEvent += _event;
+            Console.WriteLine("Some work started");
+            Thread.Sleep(1000);
+            Console.WriteLine("Some work complete");
+            someEvent(this, EventArgs.Empty);
+        }
+    }
+
+    internal class Program
+    {
+        
         private static void Main(string[] args)
         {
-            var cancellationTokenSource = new CancellationTokenSource();
-            var cancellationToken = cancellationTokenSource.Token;
-
-            Task t = Task.Run(() =>
-            {
-                while (!cancellationToken.IsCancellationRequested)
-                {
-                    Console.Write("*");
-                    Thread.Sleep(1000);
-                }
-
-                cancellationToken.ThrowIfCancellationRequested();
-            }, cancellationToken);
-
-            try
-            {
-                Console.WriteLine("Press enter to stop the task");
-                Console.Read();
-                cancellationTokenSource.Cancel();
-                t.Wait();
-            }
-            catch (AggregateException exception)
-            {
-                
-                Console.WriteLine(exception.InnerExceptions[0].Message);
-            }
-
-            Console.WriteLine("Press enter to end the application");
-            Console.ReadLine();
-
+            SomeClass myClass = new SomeClass();
+            myClass.DoSomeWork((caller,eventArgs) 
+                => Console.WriteLine("Called event raise"));
         }
     }
 }
