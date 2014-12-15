d461dea17733be7aceb50bbcbe69ae1179a28931
mkryuk <mkryuk@gmail.com>
2014-12-15 17:28:26 +0200

    Obj 1.5. try catch without finally at complete

--- 70-483/Program.cs
+++ 70-483/Program.cs
@@ -4,42 +4,22 @@ using System.Threading.Tasks;
 
 namespace _70_483
 {
-
+   
     class Program
     {
-
+       
         private static void Main(string[] args)
         {
-            var cancellationTokenSource = new CancellationTokenSource();
-            var cancellationToken = cancellationTokenSource.Token;
-
-            Task t = Task.Run(() =>
-            {
-                while (!cancellationToken.IsCancellationRequested)
-                {
-                    Console.Write("*");
-                    Thread.Sleep(1000);
-                }
-
-                cancellationToken.ThrowIfCancellationRequested();
-            }, cancellationToken);
-
+           string s = Console.ReadLine();
             try
             {
-                Console.WriteLine("Press enter to stop the task");
-                Console.Read();
-                cancellationTokenSource.Cancel();
-                t.Wait();
+                int i = int.Parse(s);
+                if (i == 42) Environment.FailFast("Special number entered");
             }
-            catch (AggregateException exception)
+            finally
             {
-                
-                Console.WriteLine(exception.InnerExceptions[0].Message);
+                Console.WriteLine("Program complete.");
             }
-
-            Console.WriteLine("Press enter to end the application");
-            Console.ReadLine();
-
         }
     }
 }
