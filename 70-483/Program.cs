89f0bb4d544eb1c59cfa0e3eaf0518a005afe01b
mkryuk <mkryuk@gmail.com>
2014-12-17 17:26:35 +0200

    Obj 2.2. DynamicObject and anonymous class with delegate

--- 70-483/Program.cs
+++ 70-483/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.Runtime.ExceptionServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -6,32 +8,74 @@ using System.Threading.Tasks;
 namespace _70_483
 {
 
-    public interface ISomeInterface
-    {
-        string Data { get; set; }
-    }
-
-    public static class SomeExtentions
-    {
-        public static string GetValue(this ISomeInterface _iface)
-        {
-            return _iface.Data;
-        }
-    }
-
-    public class SomeClass : ISomeInterface
-    {
-        public string Data { get; set; }
-    }
-
     class Program
     {
+        // The class derived from DynamicObject.
+        public class DynamicDictionary : DynamicObject
+        {
+            // The inner dictionary.
+            Dictionary<string, object> dictionary
+                = new Dictionary<string, object>();
+
+            // This property returns the number of elements
+            // in the inner dictionary.
+            public int Count
+            {
+                get
+                {
+                    return dictionary.Count;
+                }
+            }
+
+            // If you try to get a value of a property 
+            // not defined in the class, this method is called.
+            public override bool TryGetMember(
+                GetMemberBinder binder, out object result)
+            {
+                // Converting the property name to lowercase
+                // so that property names become case-insensitive.
+                string name = binder.Name.ToLower();
+
+                // If the property name is found in a dictionary,
+                // set the result parameter to the property value and return true.
+                // Otherwise, return false.
+                return dictionary.TryGetValue(name, out result);
+            }
+
+            // If you try to set a value of a property that is
+            // not defined in the class, this method is called.
+            public override bool TrySetMember(
+                SetMemberBinder binder, object value)
+            {
+                // Converting the property name to lowercase
+                // so that property names become case-insensitive.
+                dictionary[binder.Name.ToLower()] = value;
+
+                // You can always add a value to a dictionary,
+                // so this method always returns true.
+                return true;
+            }
+        }
+
+        public static dynamic GetSome()
+        {
+            return new
+            {
+                data = 10 as int?,
+                ToString = new Func<string>(()=>"some data here")
+            };
+        }
+
+       
+
         private static void Main(string[] args)
         {
-            var iface = new SomeClass();
-            iface.Data = "Some data";
+            dynamic person = new DynamicDictionary();
 
-            Console.WriteLine(iface.GetValue());
+            person.FirstName = "Ellen";
+            person.LastName = "Adams";
+
+            Console.WriteLine(person.FirstName + " " + person.LastName);
         }
     }
 }
