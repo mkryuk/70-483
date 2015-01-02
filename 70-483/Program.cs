e49722e03fa8ea778df4f00c79629102912c53bd
maxim <mkryuk@gmail.com>
2015-01-02 14:42:28 +0200

    Obj 2.5. using Lambda to generate code

--- 70-483/Program.cs
+++ 70-483/Program.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.Dynamic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.ExceptionServices;
 using System.Security.Cryptography.X509Certificates;
@@ -20,28 +21,19 @@ namespace _70_483
 
         private static void Main(string[] args)
         {
-
-            var compileUnit = new CodeCompileUnit();
-            var myNamespace = new CodeNamespace("MyNamespace");
-            myNamespace.Imports.Add(new CodeNamespaceImport("System"));
-            var myClass = new CodeTypeDeclaration("MyClass");
-            var start = new CodeEntryPointMethod();
-            var cs1 = new CodeMethodInvokeExpression(
-            new CodeTypeReferenceExpression("Console"),
-            "WriteLine", new CodePrimitiveExpression("Hello World!"));
-            compileUnit.Namespaces.Add(myNamespace);
-            myNamespace.Types.Add(myClass);
-            myClass.Members.Add(start);
-            start.Statements.Add(cs1);
-
-            var provider = new CSharpCodeProvider();
-            using (var sw = new StreamWriter("HelloWorld.cs", false))
-            {
-                var tw = new IndentedTextWriter(sw, " ");
-                provider.GenerateCodeFromCompileUnit(compileUnit, tw,
-                new CodeGeneratorOptions());
-                tw.Close();
-            }
+            var blockExpr = Expression.Block(
+                Expression.Call(
+                    null,
+                    typeof (Console).GetMethod("Write", new Type[] {typeof (String)}),
+                    Expression.Constant("Hello ")
+                    ),
+                Expression.Call(
+                    null,
+                    typeof (Console).GetMethod("WriteLine", new Type[] {typeof (String)}),
+                    Expression.Constant("World!")
+                    )
+                );
+            Expression.Lambda<Action>(blockExpr).Compile()();
         }
     }
 }
