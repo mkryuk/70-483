70fa9380f7685ecd3190b3cb5f60b394b8ddfc40
maxim <mkryuk@gmail.com>
2015-01-26 20:31:44 +0200

    Obj 3.2 symmetric encryption/decryption

--- 70-483/Program.cs
+++ 70-483/Program.cs
@@ -9,116 +9,68 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.ExceptionServices;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CSharp;
+using System.Web.Script.Serialization;
 
 namespace _70_483
 {
-    public class SomeDisposableClass : IDisposable
-    {
-        private WeakReference _data;
-        private bool disposed;
-
-        public void PrintData()
-        {
-            Thread.Sleep(1000);
-            Console.WriteLine("Data {0}",_data);
-        }
-
-        public SomeDisposableClass()
-        {
-            //pre caching data to the weak reference
-            var result = GetData();
-        }
-
-        public void Run()
-        {
-            //pre caching data to the weak reference
-            var result = GetData();
-        }
-
-        public  object GetData()
-        {
-            if (_data == null)
-            {
-                _data = new WeakReference(LoadLargeList());
-            }
-            return _data.Target ?? (_data.Target = LoadLargeList());
-        }
-
-        private  List<int> LoadLargeList()
-        {
-            Console.WriteLine("Loading the large list");
-            var data = new List<int>();
-            for (var i = 0; i < 1000000; i++)
-            {
-                data.Add(i);
-            }
-            return data;
-        }
-        //Dispose pattern for managed resources
-        protected void Dispose(bool disposing)
-        {
-            if (disposed) return;
-            if (disposing)
-            {
-                _data = null;
-                Console.WriteLine("Object disposed");
-            }
-            disposed = true;
-        }
-
-        public void Dispose()
-        {
-            Console.WriteLine("Dispose called");
-            Dispose(true);
-            //supress finalize method for not to keep reference in GC Finalization list
-            GC.SuppressFinalize(this);
-        }
-        //Finalize method for unmanaged resources
-        ~SomeDisposableClass()
-        {
-            //finalize method must call Dispose(false)
-            Dispose(false);
-            Console.WriteLine("Destructor called from {0} thread", Thread.CurrentThread.ManagedThreadId);
-        }
-    }
-
+  
     public class Program
     {
+        public static void EncryptSomeText(string data)
+        {
+            var original = data;
+            using (SymmetricAlgorithm symmetricAlgorithm = new AesManaged())
+            {
+                var encrypted = Encrypt(symmetricAlgorithm, original);
+                var decrypted = Decrypt(symmetricAlgorithm, encrypted);
+
+                Console.WriteLine("Original: {0}",original);
+                Console.WriteLine("Encrypted: {0}",Encoding.UTF8.GetString(encrypted));
+                Console.WriteLine("Decrypted: {0}", decrypted);
+            }
+        }
+
+        private static string Decrypt(SymmetricAlgorithm aesAlg, byte[] cipherText)
+        {
+            var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+            using (var msDecryption = new MemoryStream(cipherText))
+            {
+                using (var csDecrypt = new CryptoStream(msDecryption,decryptor,CryptoStreamMode.Read))
+                {
+                    using (var srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        public static byte[] Encrypt(SymmetricAlgorithm aesAlg, string plainText)
+        {
+            var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+            using (var msEncryption = new MemoryStream())
+            {
+                using (var csEncrypt = new CryptoStream(msEncryption,encryptor,CryptoStreamMode.Write))
+                {
+                    using (var swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                    return msEncryption.ToArray();
+                }
+            }
+        }
 
         private static void Main(string[] args)
         {
-            var disposableObject = new SomeDisposableClass();
-            var someThread = new Thread(ThreadMethod);
-            someThread.Start(disposableObject);
-            disposableObject.PrintData();
-
-            //using (var disposableNewObject = new SomeDisposableClass(12))
-            //{
-            //    //due to dispose pattern resources frees only once
-            //    disposableObject.Dispose();
-            //}
-
-            //next line removes the data from weak reference cache
-            //GC.Collect();
-            disposableObject.Run();
-
-            //next line removes the data from weak reference cache
-            //GC.Collect();
-            var someNewResult = disposableObject.GetData();
+           EncryptSomeText("My secret data");
         }
 
-        private static void ThreadMethod(object obj)
-        {
-            Thread.Sleep(1000);
-            var someDisposableClass = obj as SomeDisposableClass;
-            if (someDisposableClass == null) return;
-
-            someDisposableClass.PrintData();
-            new Thread(someDisposableClass.PrintData).Start();
-        }
     }
 }
